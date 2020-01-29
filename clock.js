1094beb3126eb52ef71f74b150d262f2ca5d20e4
hold560 <30383969+hold560@users.noreply.github.com>
2020-01-29 09:57:48 +0900

    29 JAN
    creat a clock on real time base

---
+++ clock.js
@@ -0,0 +1,24 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+//1)load the clock
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+
+  //2) print time on title and adjust the format to print time
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${
+    seconds < 10 ? `0${seconds}` : seconds // mini if applied
+  }`;
+}
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+  //setInterval(function, miliseconds)
+}
+
+init();
